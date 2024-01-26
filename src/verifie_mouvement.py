152cb217f36d4fbb6c1a7e6793aa2e68016100a3
creismeas Maël <mael.creismeas@outlook.fr>
2024-01-26 15:51:34 +0100

    update

--- src/verifie_mouvement.py
+++ src/verifie_mouvement.py
@@ -1,4 +1,13 @@
 from src.person import Player
+from src.CaseVide import CaseVide
+from src.Mur import Mur
+from src.PorteFerme import PorteFerme
+from src.PorteOuverte import PorteOuverte
+from src.BarriereGauche import BarriereGauche
+from src.BarriereDroite import BarriereDroite
+from src.BarriereBas import BarriereBas
+from src.BarriereHaut import BarriereHaut
+
 
 def verifie_mouvement(map, joueur, direction):
 
@@ -6,21 +15,22 @@ def verifie_mouvement(map, joueur, direction):
     y_position_joueur=joueur.y
 
     tuille = map[x_position_joueur][y_position_joueur]
-    if (tuille==direction+1):
+    if (tuille==direction+4):
+        
         if(tuille=="0"):    
-                                        map_a_print+=(chr(46))
+            case=CaseVide()
         elif(tuille=="1"):    
-                                        map_a_print+=(chr(64))
+            case=Mur()
         elif(tuille=="2"):    
-                                        map_a_print+=(chr(21))
+            case=PorteFerme()
         elif(tuille=="3"):    
-                                        map_a_print+=(chr(47))
+            case=PorteOuverte()
         elif(tuille=="4"):    
-                                        map_a_print+=(chr(27))
+            case=BarriereGauche()
         elif(tuille=="5"):    
-                                        map_a_print+=(chr(26))
+            case=BarriereDroite()
         elif(tuille=="6"):    
-                                        map_a_print+=(chr(25))
+            case=BarriereBas()
         elif(tuille=="7"):    
                                         map_a_print+=(chr(24))
         elif(tuille=="8"):    
@@ -40,8 +50,7 @@ def verifie_mouvement(map, joueur, direction):
         elif(tuille=="15"):    
                                         map_a_print+=(chr(19))
         elif(tuille=="16"):    
-                                        map_a_print+=(chr(23))
-    
+                                           map_a_print+=(chr(23))
 
 
 
